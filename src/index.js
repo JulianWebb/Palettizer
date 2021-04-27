52beac51ebe35e9e0070200ad4abbdb094ba9c6e
Julian Webb <git@julianwebb.ca>
2021-04-27 14:12:57 -0700

    .......

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const Palettizer = require(__dirname + '/palettizer');
 const Logger = require(__dirname + '/logger');
+const axios = require('axios');
 
 const palettizer = new Palettizer({}, new Logger({ name: "Palettizer"}));
 palettizer.listen(process.env.APP_PORT);
 console.log(`Listening on ${process.env.APP_PORT}`)
+
+axios.get('http://papermerge:8000/')
+    .then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.error(error);
+    })
